453f73d2d5c33cb3e1cebaacca5f1be3369e122f
du-nt <mahoagray@gmail.com>
2024-05-19 23:43:18 +0900

    first commit

--- counter.groovy
+++ counter.groovy
@@ -1,10 +1,3 @@
-
-def credentials = CredentialsProvider.lookupCredentials(
-    StandardUsernamePasswordCredentials.class, 
-    Jenkins.instance
-);
-
-// def cred = credentials.findResult { it.id == "text" ? it : null }
 def printCounter(String param) {
     echo "Common function called with parameter: ${param}"
 
@@ -13,10 +6,31 @@ def printCounter(String param) {
     
     env.NAME = "hello cong san"
     echo "The vdfsdfdsfE_VAR is: ${env.NAME}"
-    echo "Txxxxxxxxxxxxxxxxxxxxx ${credentials}"
+
     // Return some parameters
     return [status: 'SUCCESS', message: 'Function executed successfully']
 }
 
+import com.cloudbees.plugins.credentials.Credentials
+import com.cloudbees.plugins.credentials.CredentialsProvider
+
+Set<Credentials> allCredentials = new HashSet<Credentials>();
+Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class).each{ f ->
+ creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+      com.cloudbees.plugins.credentials.Credentials.class, f)
+  allCredentials.addAll(creds)
+
+}
+for (c in allCredentials) {
+  if (CredentialsProvider.FINGERPRINT_ENABLED) {
+    fp = CredentialsProvider.getFingerprintOf(c)
+    if (fp) {  
+    	fp_str = "Fingerprinted jobs: " + fp.getJobs()
+  	} else {
+    	fp_str = "(No Fingerprints)"
+    }  
+  }
+  println(c.id + " : " + c.description  + " | " + fp_str)
+}
 
 return this;
