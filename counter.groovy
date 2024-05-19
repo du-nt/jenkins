2727577c09f5e16005146442553bb85403d80809
du-nt <mahoagray@gmail.com>
2024-05-19 22:55:42 +0900

    first commit

--- counter.groovy
+++ counter.groovy
@@ -1,5 +1,22 @@
 def printCounter(String param) {
     echo "Common function called with parameter: ${param}"
+
+     environment {
+        EXAMPLE_VAR = 'Hello, World!'
+    }
+
+    stage('GCPコマンド認証') {
+      steps{
+        script{
+          withCredentials([string(credentialsId: 'text', variable: 'GC_KEY')]) {
+            sh 'echo $GC_KEY'
+	        }
+        
+        echo "The value of EXAMPLE_VAR is: ${env.EXAMPLE_VAR}"
+        }
+      }
+    }
+
     // Return some parameters
     return [status: 'SUCCESS', message: 'Function executed successfully']
 }
