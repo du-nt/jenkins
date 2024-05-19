03e3f28e70190edc52e0f6c11dff8bf57e374eb5
du-nt <mahoagray@gmail.com>
2024-05-19 21:05:30 +0900

    first commit

---
+++ commonFunction.groovy
@@ -0,0 +1,5 @@
+def call(String param) {
+    echo "Common function called with parameter: ${param}"
+    // Return some parameters
+    return [status: 'SUCCESS', message: 'Function executed successfully']
+}
