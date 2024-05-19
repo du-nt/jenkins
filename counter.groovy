e9812f180908d94dd315e22064b2658e82b6cc39
du-nt <mahoagray@gmail.com>
2024-05-19 23:17:53 +0900

    first commit

--- counter.groovy
+++ counter.groovy
@@ -1,9 +1,9 @@
-def envVar = System.getenv('env_ne')
 
 def printCounter(String param) {
     echo "Common function called with parameter: ${param}"
 
-    echo "The value of EXAMPLE_VAR is: ${envVar}"
+    def exampleVar = env.env_ne
+    echo "The value of EXAMPLE_VAR is: ${exampleVar}"
     
     // Return some parameters
     return [status: 'SUCCESS', message: 'Function executed successfully']
