6df888d5d7a9a2298fe93ddd03ef960e92a7b7a6
du-nt <mahoagray@gmail.com>
2024-05-19 23:39:05 +0900

    first commit

--- counter.groovy
+++ counter.groovy
@@ -1,3 +1,10 @@
+
+def credentials = CredentialsProvider.lookupCredentials(
+    StandardUsernamePasswordCredentials.class, 
+    Jenkins.instance
+);
+
+def cred = credentials.findResult { it.id == "text" ? it : null }
 def printCounter(String param) {
     echo "Common function called with parameter: ${param}"
 
@@ -5,9 +12,11 @@ def printCounter(String param) {
     echo "The value of EXAMPLE_VAR is: ${exampleVar}"
     
     env.NAME = "hello cong san"
-    echo "The vdfsdfdsfE_VAR is: ${env.NAME}"
+    echo "The vdfsdfdsfE_VAR is: ${NAME}"
+    echo "Txxxxxxxxxxxxxxxxxxxxx ${cred}"
     // Return some parameters
     return [status: 'SUCCESS', message: 'Function executed successfully']
 }
+
 
 return this;
